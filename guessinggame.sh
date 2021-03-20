c5d049a55421adc177865e098726eb721ec17fd9
jyothigogineni <80961855+jyothigogineni@users.noreply.github.com>
2021-03-20 17:47:59 +0530

    Create guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,20 @@
+guessdir()
+{
+        count=$(ls -l | wc -l )
+while read  -p "Enter the guess of files in current directory:" attempt
+do
+
+
+        if [ $attempt -lt $count ]
+        then echo "too low value "
+        elif [ $attempt -gt $count ]
+        then echo "too high value"
+        else
+
+                echo "your guess is right"
+                break
+        fi
+done
+}
+        guessdir
+        echo "total number of files in the current directory: $count"
